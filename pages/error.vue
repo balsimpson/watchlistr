4a251bd7a6204bb5c6efe5bfc3d57ce64835c0d4
balsipson <thirdi@gmail.com>
2023-05-30 14:59:56 +0530

    trying to add custom domain to firebase

---
+++ pages/error.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="max-w-[50rem] flex flex-col mx-auto w-full h-full mt-12">
+
+
+		<div class="px-4 py-10 text-center sm:px-6 lg:px-8">
+			<h1
+				class="block font-bold text-gray-800 text-7xl sm:text-9xl dark:text-white"
+			>
+				404
+			</h1>
+			<h1 class="block text-2xl font-bold text-white"></h1>
+			<p class="mt-3 text-gray-600 dark:text-gray-400">
+				Oops, something went wrong.
+			</p>
+			<p class="text-gray-600 dark:text-gray-400">
+				Sorry, we couldn't find your page.
+			</p>
+			<div
+				class="flex flex-col items-center justify-center gap-2 mt-5 sm:flex-row sm:gap-3"
+			>
+
+				<a
+					class="inline-flex items-center justify-center w-full gap-2 px-4 py-3 text-sm font-semibold text-blue-500 transition-all border border-transparent rounded-md sm:w-auto hover:text-blue-700 focus:outline-none focus:ring-2 ring-offset-white focus:ring-blue-500 focus:ring-offset-2 dark:ring-offset-slate-900"
+					href="/"
+				>
+					<svg
+						class="w-2.5 h-2.5"
+						width="16"
+						height="16"
+						viewBox="0 0 16 16"
+						fill="none"
+					>
+						<path
+							d="M11.2792 1.64001L5.63273 7.28646C5.43747 7.48172 5.43747 7.79831 5.63273 7.99357L11.2792 13.64"
+							stroke="currentColor"
+							stroke-width="2"
+							stroke-linecap="round"
+						/>
+					</svg>
+					Back to Home
+				</a>
+			</div>
+		</div>
+	</div>
+</template>
